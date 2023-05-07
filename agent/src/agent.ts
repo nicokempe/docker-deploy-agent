dc8a1d4f91fca262633370794e7e74c665c19a4f
Nico Kempe <accounts@nicokempe.de>
2023-05-07 16:21:16 +0200

    Improve GitHub integration in agent.ts Utility
    - Add parameters for repoOwner and gitHubToken in getLatestImage function
    - Update error handling for cases when no latest image is found

--- agent/src/agent.ts
+++ agent/src/agent.ts
@@ -4,23 +4,29 @@ import { getLatestImage } from './utils/github';
 
 async function main() {
     try {
-        const latestImage = await getLatestImage(config.repoName, config.token);
-        const updateRequired = await needsUpdate(latestImage, config.packageName);
+        const latestImage = await getLatestImage(config.repoOwner, config.repoName, config.gitHubToken);
 
-        if (updateRequired) {
-            try {
-                await deployNewImage(latestImage);
-                console.log('The Update was successful!');
-            } catch (error) {
-                console.error('Update failed, rolling back:', error);
-                await rollback();
-                console.log('The Rollback was successful!');
+        if (latestImage) {
+            const updateRequired = await needsUpdate(latestImage, config.packageName);
+
+            if (updateRequired) {
+                try {
+                    await deployNewImage(latestImage);
+                    console.log('The Update was successful!');
+                } catch (error) {
+                    console.error('Update failed, rolling back:', error);
+                    await rollback();
+                    console.log('The Rollback was successful!');
+                }
             }
+        } else {
+            console.log('No latest image found');
         }
     } catch (error) {
         console.error('An error occurred during the update check:', error);
     }
 }
+
 
 (async function runAgent() {
     while (true) {

a271a81e32d42e91b4a1e7f5cb67b5a7913b128b
Nico Kempe <accounts@nicokempe.de>
2023-05-07 16:18:02 +0200

    Update GitHub Utility with improved functionality
    - Add new parameter for GitHub personal access token
    - Update 'Authorization' header to use the updated parameter
    - Update return value to include repoOwner and latest Docker image tag
    - Addition of 'gitHubToken:string' parameter resolves authorization issues during image pull

--- agent/src/utils/github.ts
+++ agent/src/utils/github.ts
@@ -3,14 +3,14 @@ import { config } from '../config';
 
 const githubApiBaseUrl = 'https://api.github.com';
 
-export async function getLatestImage(repoOwner: string, repoName: string): Promise<string | null> {
+export async function getLatestImage(repoOwner: string, repoName: string, gitHubToken:string): Promise<string | null> {
     try {
         const response = await axios.get(
             `${githubApiBaseUrl}/orgs/${repoOwner}/packages/${config.packageType}/${config.packageName}/versions`,
             {
                 headers: {
                     Accept: 'application/vnd.github+json',
-                    Authorization: `token ${config.token}`,
+                    Authorization: `token ${config.gitHubToken}`,
                 },
                 params: {
                     per_page: 1,
@@ -18,13 +18,13 @@ export async function getLatestImage(repoOwner: string, repoName: string): Promi
                 },
             }
         );
-
         const latestVersion = response.data[0];
         if (!latestVersion) {
             return null;
         }
 
-        return `${config.packageName}@${latestVersion.name}`;
+        return `${repoOwner}/${config.packageName}:${latestVersion.metadata.container.tags[0]}`;
+
     } catch (error) {
         console.error('An error occured fetching the latest image: ', error);
         throw error;

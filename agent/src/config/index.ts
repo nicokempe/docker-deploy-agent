3557348bf8a4074aab50d46a228201505e4bb164
Nico Kempe <accounts@nicokempe.de>
2023-05-07 16:14:04 +0200

    Update .env entries and add documentation link
    - Updated values for GITHUB_TOKEN, GITHUB_REPO_OWNER, GITHUB_REPO_NAME, PACKAGE_NAME, CHECK_INTERVAL, and DOCKER_AGENT_IMAGE
    - Added new environment variables for GITHUB_USER_NAME and GITHUB_USER_MAIL
    - Added documentation link to the repository's environment variables in the .env file

--- agent/src/config/index.ts
+++ agent/src/config/index.ts
@@ -1,12 +1,18 @@
 import dotenv from 'dotenv';
 
+// Docs: https://github.com/nicokempe/docker-deploy-agent/wiki/Enviroment-variables
+
 dotenv.config();
 
 export const config = {
-    token: process.env.GITHUB_TOKEN || '',
+    gitHubUsername: process.env.GITHUB_USER_NAME || '',
+    gitHubUserMail: process.env.GITHUB_USER_MAIL || '',
+    gitHubToken: process.env.GITHUB_TOKEN || '',
     repoOwner: process.env.GITHUB_REPO_OWNER || '',
     repoName: process.env.GITHUB_REPO_NAME || '',
     packageName: process.env.PACKAGE_NAME || '',
-    packageType: process.env.PACKAGE_TYPE || '',
-    interval: parseInt(process.env.CHECK_INTERVAL || '900000', 10),
+    packageType: process.env.PACKAGE_TYPE || 'container',
+    interval: Number(process.env.CHECK_INTERVAL) || 900000,
+    dockerAgentImage: process.env.DOCKER_AGENT_IMAGE || '',
+    dockerAgentTag: process.env.DOCKER_AGENT_TAG || 'latest',
 };
